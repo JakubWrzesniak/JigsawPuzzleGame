6d617b842ebc5e14a38ea705b6fb80ee18b998ad
JakubWrzesniak <jakub.wrzesniak@outlook.com>
2022-04-12 20:59:59 +0200

    Initial Commit

---
+++ JigsawPuzzleGame macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  JigsawPuzzleGame macOS
+//
+//  Created by Jakub Wrześniak on 12/04/2022.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
